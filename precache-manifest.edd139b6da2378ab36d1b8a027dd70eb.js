84434f8141ee5d2b50fa3f2079681c8e522927a7
GretaRob <58323636+GretaRob@users.noreply.github.com>
2020-06-15 16:04:45 +0700

    Updates

---
+++ precache-manifest.edd139b6da2378ab36d1b8a027dd70eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11aaba3cd126ef1dc81cb06b4e0a35d5",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "b94998b8b2e79b54a5c0",
+    "url": "/calculator/static/css/main.a94e9091.chunk.css"
+  },
+  {
+    "revision": "d5a6f929632f121958a1",
+    "url": "/calculator/static/js/2.f5432d81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.f5432d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94998b8b2e79b54a5c0",
+    "url": "/calculator/static/js/main.ea553002.chunk.js"
+  },
+  {
+    "revision": "2d2b92a0757cd2e07515",
+    "url": "/calculator/static/js/runtime-main.684c6b37.js"
+  },
+  {
+    "revision": "e1cf7978ad2a06fae37d7e84cb30d371",
+    "url": "/calculator/static/media/pattern.e1cf7978.jpg"
+  }
+]);
